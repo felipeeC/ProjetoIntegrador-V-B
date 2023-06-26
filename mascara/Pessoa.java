19b338acba66205af5cdc2784bff013671d76ec8
FernandoReisG <95513373+FernandoReisG@users.noreply.github.com>
2023-06-25 23:36:10 -0300

    Add files via upload
    metodo de criptografia de login

---
+++ mascara/Pessoa.java
@@ -0,0 +1,131 @@
+package br.com.compasso.lambda.desafioCompasso.models;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Pessoa {
+	
+	@Id	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	private LocalDate aniversario;
+	private String email;
+
+	@ManyToMany(mappedBy = "pessoas")
+	private List<Filme> filmes = new ArrayList<>();
+	// Construtor
+
+	public Pessoa() {
+		super();
+	}
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+}
+
+    public class LoginRequest {
+    private String email;
+    private String password;
+
+    // getters e setters
+}
+
+
+	public Pessoa(String nome, LocalDate aniversario, String email) {
+		this.nome = nome;
+		this.aniversario = aniversario;
+		this.email = email;
+	}
+
+	// Getters e Setters
+
+	public List<Filme> getFilmes() {
+		return filmes;
+	}
+
+	public void setFilmes(List<Filme> filmes) {
+		this.filmes = filmes;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getAniversario() {
+		return aniversario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setAniversario(LocalDate aniversario) {
+		this.aniversario = aniversario;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Pessoa [id=" + id + 
+				", nome=" + nome + 
+				", aniversario=" + aniversario + 
+				", email=" + email + 
+				", filmes="	+ filmes + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((aniversario == null) ? 0 : aniversario.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (aniversario == null) {
+			if (other.aniversario != null)
+				return false;
+		} else if (!aniversario.equals(other.aniversario))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+}
---
+++ mascara/PessoaController.java
@@ -0,0 +1,122 @@
+package br.com.compasso.lambda.desafioCompasso.controllers;
+
+import java.net.URI;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import br.com.compasso.lambda.desafioCompasso.dtos.PessoaAtualizaForm;
+import br.com.compasso.lambda.desafioCompasso.dtos.PessoaDto;
+import br.com.compasso.lambda.desafioCompasso.dtos.PessoaForm;
+import br.com.compasso.lambda.desafioCompasso.models.Pessoa;
+import br.com.compasso.lambda.desafioCompasso.services.PessoaService;
+
+@RestController
+@RequestMapping(value = "/pessoas")
+public class PessoaController {
+
+	@Autowired
+	private PessoaService pessoaService;
+
+	@GetMapping
+	public ResponseEntity<Page<PessoaDto>> todasPessoas(
+			@PageableDefault(
+					sort = "id", 
+					direction = Direction.ASC, 
+					page = 0, 
+					size = 10) Pageable paginacao
+			) {
+		
+		Page<PessoaDto> pessoas = pessoaService.retornaTodas(paginacao);
+		return ResponseEntity.ok(pessoas);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<PessoaDto> pessoaById(@PathVariable Long id) {
+		Pessoa pessoa = pessoaService.findById(id);
+		return ResponseEntity.ok(new PessoaDto(pessoa));
+	}
+    @PostMapping("/login")
+public ResponseEntity<String> login(@RequestBody @Valid LoginRequest loginRequest) {
+    Pessoa pessoa = pessoaService.findByEmail(loginRequest.getEmail());
+
+    if (pessoa == null) {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário não encontrado");
+    }
+
+    BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+    if (encoder.matches(loginRequest.getPassword(), pessoa.getPassword())) {
+        return ResponseEntity.ok("Login realizado com sucesso");
+    } else {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Senha incorreta");
+    }
+}
+
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<PessoaDto> cadastrarPessoa(
+			@RequestBody @Valid PessoaForm form,
+			UriComponentsBuilder uriBuilder
+			) {
+		PessoaDto novaPessoa = pessoaService.cadastrarPessoa(form);
+		if(novaPessoa == null) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).build();
+		}
+		// Criptografar a senha antes de salvar
+		 BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		 novaPessoa.setPassword(encoder.encode(novaPessoa.getPassword()));
+
+		URI uri = uriBuilder
+				.path("/pessoas/{id}")
+				.buildAndExpand(novaPessoa.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).body(novaPessoa);
+	}
+	
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<PessoaDto> atualizarPessoa(
+			@PathVariable Long id,
+			@RequestBody @Valid PessoaAtualizaForm form
+			) {
+		PessoaDto pessoaAtualizada = pessoaService.atualizarPessoa(id, form);
+		if(pessoaAtualizada == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(pessoaAtualizada);
+		
+	}
+	
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity<?> deleteById(
+			@PathVariable Long id
+			){
+		pessoaService.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+
+}
+
+
